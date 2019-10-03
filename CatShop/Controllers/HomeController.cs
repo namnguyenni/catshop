c05401d850aa6cacff95551ff8629d158b0c4f45
DoVanHao <DoVanHao@users.noreply.github.com>
2019-10-03 06:22:17 -0700

    hao gui

--- CatShop/Controllers/HomeController.cs
+++ CatShop/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CatShop.Models;
+using System.Linq;
 
 
 namespace CatShop.Controllers
@@ -15,34 +16,120 @@ namespace CatShop.Controllers
 
         public ActionResult Index()
         {
-            CAT_SHOP1Entities data = new CAT_SHOP1Entities();
+
             MultiCustomModel multicus = new MultiCustomModel();
-            var cattable = data.Meos;
 
-            List<CatCustomModel> listcats = new List<CatCustomModel>();
-            foreach (var item in cattable)
-            {
-                listcats.Add(new CatCustomModel
-                {
-                    
-                    Id = item.Id,
-                    img = item.img
-                    
-                });
-
-                
-                
-            }
-            
-            multicus.ModelCAT_Trending.AddRange(listcats);
-
-            
-
-
+            multicus.ModelCAT_CarouselStart.AddRange(GetCatCarouselStart());
+            multicus.ModelCAT_Trending.AddRange(GetSellCatTrending());
+            multicus.ModelCAT_BestSeller.AddRange(GetSellCatBestSeller());
+            multicus.ModelCAT_BlogSection.AddRange(GetSellCatBlogSection());
             return View(multicus);
         }
 
+        public List<CatCustomModel> GetCatCarouselStart()
+        {
+            List<CatCustomModel> listcats = new List<CatCustomModel>();
+            CAT_SHOP1Entities data = new CAT_SHOP1Entities();
+            var cattable = data.BestCats;
+            var cattable1 = data.Meos;
+            var item = (from a in cattable
+                        join b in cattable1 on a.Id equals b.Id
+                        where a.Check_cat == 1
+                        select new
+                        {
+                            id = b.Id,
+                            img = b.img
+                        }).ToList();
 
+            foreach (var i in item)
+            {
+                listcats.Add(new CatCustomModel
+                {
+                    Id = i.id,
+                    img = i.img
+                });
+            }
+            return listcats;
+        }
+
+        public List<CatCustomModel> GetSellCatTrending()
+        {
+            List<CatCustomModel> listcats = new List<CatCustomModel>();
+            CAT_SHOP1Entities data = new CAT_SHOP1Entities();
+            var cattable = data.MaxMonneyCats;
+            var cattable1 = data.Meos;
+            var item = (from a in cattable
+                        join b in cattable1 on a.Id equals b.Id
+                        where a.Check_cat == 1
+                        select new
+                        {
+                            id = b.Id,
+                            img = b.img
+                        }).ToList();
+
+            foreach (var i in item)
+            {
+                listcats.Add(new CatCustomModel
+                {
+                    Id = i.id,
+                    img = i.img
+                });
+            }
+            return listcats;
+        }
+
+
+        public List<CatCustomModel> GetSellCatBestSeller()
+        {
+            List<CatCustomModel> listcats = new List<CatCustomModel>();
+            CAT_SHOP1Entities data = new CAT_SHOP1Entities();
+            var cattable = data.SellerCats;
+            var cattable1 = data.Meos;
+            var item = (from a in cattable
+                        join b in cattable1 on a.Id equals b.Id
+                        where a.Check_cat == 1
+                        select new
+                        {
+                            id = b.Id,
+                            img = b.img
+                        }).ToList();
+
+            foreach (var i in item)
+            {
+                listcats.Add(new CatCustomModel
+                {
+                    Id = i.id,
+                    img = i.img
+                });
+            }
+            return listcats;
+        }
+
+        public List<CatCustomModel> GetSellCatBlogSection()
+        {
+            List<CatCustomModel> listcats = new List<CatCustomModel>();
+            CAT_SHOP1Entities data = new CAT_SHOP1Entities();
+            var cattable = data.LastNews;
+            var cattable1 = data.Meos;
+            var item = (from a in cattable
+                        join b in cattable1 on a.Id equals b.Id
+                        where a.Check_cat == 1
+                        select new
+                        {
+                            id = b.Id,
+                            img = b.img
+                        }).ToList();
+
+            foreach (var i in item)
+            {
+                listcats.Add(new CatCustomModel
+                {
+                    Id = i.id,
+                    img = i.img
+                });
+            }
+            return listcats;
+        }
 
 
         public ActionResult Shop()
---
+++ CatShop/Models/BestCatCustom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CatShop.Models
+{
+    public class BestCatCustom
+    {
+        public string Id1 { get; set; }
+        public int Check_cat { get; set; }
+        public string Id { get; set; }
+
+        public virtual Meo Meo { get; set; }
+    }
+}
